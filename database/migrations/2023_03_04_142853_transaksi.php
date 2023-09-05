8e795179954759732b2d3151961d7ed65f60b899
NathanaelGracedo <nathanjuan56@gmail.com>
2023-09-05 07:45:25 +0700

    initial commit

---
+++ database/migrations/2023_03_04_142853_transaksi.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('tgl_transaksi', $precision = 0);
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('id_meja');
+            $table->foreign('id_meja')->references('id')->on('meja')->onDelete('cascade');
+            $table->string('nama_pelanggan', 100);
+            $table->enum('status',['belum_bayar', 'lunas']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksi');
+    }
+};
